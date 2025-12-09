0d1f530bb2cc658df60e6ee050d4e555828bf384
Arvin Garcia <arvin@arvingarcia.com>
2025-12-09 08:10:28 +0800

    feat: style error page

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,15 +1,20 @@
+import styles from "../styles/error-page.module.css";
 import { useRouteError, isRouteErrorResponse } from "react-router";
+import { Link } from "react-router";
 
 function ErrorBoundary() {
 	const error = useRouteError();
 	if (isRouteErrorResponse(error)) {
 		return (
-			<>
-				<h1>
+			<main className={styles.main}>
+				<h1 className={styles.errorHeading}>
 					{error.status} {error.statusText}
 				</h1>
 				<p>{error.data}</p>
-			</>
+				<Link to="/" className={styles.back}>
+					Back to home
+				</Link>
+			</main>
 		);
 	} else if (error instanceof Error) {
 		return (
