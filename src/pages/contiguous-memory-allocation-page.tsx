2061a64f44b2b8b8ea8ce830ed4714c43c8fcd8c
Arvin Garcia <arvin@arvingarcia.com>
2025-12-10 11:21:09 +0800

    feat: add contiguous memory allocation base layout

--- src/pages/contiguous-memory-allocation-page.tsx
+++ src/pages/contiguous-memory-allocation-page.tsx
@@ -1,16 +1,35 @@
 import styles from "../styles/contiguous-memory-allocation.module.css";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
+import Information from "../components/information";
 
 function CMAPage() {
 	return (
 		<>
 			<NavBar />
-			<main className={`maxWidthWrapper ${styles.main}`}>
-				The simulation here.
-			</main>
+			<Information
+				title="Contiguous Memory Allocation"
+				description="
+          A visualizer for contiguous memory allocation. Provide the parameters
+          you want, and specify your chosen algorithm on the top left.
+        "
+			/>
+			<Main />
 			<Footer />
 		</>
+	);
+}
+
+function Main() {
+	return (
+		<main className={`maxWidthWrapper ${styles.main}`}>
+			<section className={styles.algorithms}>Algorithms</section>
+			<section className={styles.processes}>Processes</section>
+			<section className={styles.parameters}>Parameters</section>
+			<section className={styles.players}>Players</section>
+			<section className={styles.chart}>Chart</section>
+			<section className={styles.simulation}>Simulation</section>
+		</main>
 	);
 }
 
