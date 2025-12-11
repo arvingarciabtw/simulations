fb3b58ef54c408eef35dffa53bcb9349c642dfee
Arvin Garcia <arvin@arvingarcia.com>
2025-12-11 11:01:57 +0800

    feat: implement algorithms component

--- src/pages/contiguous-memory-allocation-page.tsx
+++ src/pages/contiguous-memory-allocation-page.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/contiguous-memory-allocation.module.css";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 import Information from "../components/information";
+import { Select } from "radix-ui";
 
 function CMAPage() {
 	return (
@@ -23,13 +24,49 @@ function CMAPage() {
 function Main() {
 	return (
 		<main className={`maxWidthWrapper ${styles.main}`}>
-			<section className={styles.algorithms}>Algorithms</section>
 			<section className={styles.processes}>Processes</section>
 			<section className={styles.parameters}>Parameters</section>
 			<section className={styles.players}>Players</section>
 			<section className={styles.chart}>Chart</section>
 			<section className={styles.simulation}>Simulation</section>
+			<Algorithms />
 		</main>
+	);
+}
+
+function Algorithms() {
+	return (
+		<section className={styles.algorithms}>
+			<h2 className={styles.sectionTitle}>Algorithm</h2>
+			<Select.Root>
+				<Select.Trigger className={styles.selectTrigger} aria-label="Algorithm">
+					<Select.Value className={styles.selectValue} placeholder="..." />
+					<Select.Icon>
+						<ChevronDown className={styles.chevronDown} />
+					</Select.Icon>
+				</Select.Trigger>
+				<Select.Portal>
+					<Select.Content className={styles.selectContent}>
+						<Select.ScrollUpButton />
+						<Select.Viewport className={styles.selectViewport}>
+							<Select.Group>
+								<Select.Item className={styles.selectItem} value="first-fit">
+									<Select.ItemText>First-fit</Select.ItemText>
+								</Select.Item>
+								<Select.Item className={styles.selectItem} value="best-fit">
+									<Select.ItemText>Best-fit</Select.ItemText>
+								</Select.Item>
+								<Select.Item className={styles.selectItem} value="worst-fit">
+									<Select.ItemText>Worst-fit</Select.ItemText>
+								</Select.Item>
+							</Select.Group>
+						</Select.Viewport>
+
+						<Select.ScrollUpButton />
+					</Select.Content>
+				</Select.Portal>
+			</Select.Root>
+		</section>
 	);
 }
 
