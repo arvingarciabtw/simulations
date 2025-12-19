02fc976aea0c4ebdc2bc78ea519e5f3cbb9ab889
Arvin Garcia <arvin@arvingarcia.com>
2025-12-19 12:53:53 +0800

    feat: add simple input validation for player click

--- src/components/contiguous-memory-allocation/players.tsx
+++ src/components/contiguous-memory-allocation/players.tsx
@@ -22,36 +22,39 @@ export default function Players({
 	const [showToast, setShowToast] = useState(false);
 	const [toastMessage, setToastMessage] = useState("");
 
-	function handleClick(message: string) {
+	function handleClick() {
+		const hasAlgorithm = algorithm !== "...";
+		const hasParameters =
+			parameters.memorySize > 0 &&
+			parameters.compactionTime > 0 &&
+			parameters.coalescingTime > 0;
+		const hasProcess = Boolean(processes.length > 0);
+
 		setShowToast(true);
-		setToastMessage(
-			message + algorithm + parameters.memorySize + processes[0].name,
-		);
+		if (hasAlgorithm && hasParameters && hasProcess) {
+			setToastMessage("Can be simulated!");
+		} else {
+			setToastMessage(
+				"Make sure you have at least one process, and all inputs have valid values.",
+			);
+		}
+
 		setTimeout(() => {
 			setShowToast(false);
-		}, 3000);
+		}, 5000);
 	}
 
 	return (
 		<section className={styles.playersGridArea}>
 			<h2 className={styles.sectionTitle}>Players</h2>
 			<div className={styles.playersContainer}>
-				<button
-					className={styles.btnReset}
-					onClick={() => handleClick("Clicked reset!")}
-				>
+				<button className={styles.btnReset} onClick={handleClick}>
 					<RotateCw />
 				</button>
-				<button
-					className={styles.btnForward}
-					onClick={() => handleClick("Clicked forward player!")}
-				>
+				<button className={styles.btnForward} onClick={handleClick}>
 					<Play />
 				</button>
-				<button
-					className={styles.btnFastForward}
-					onClick={() => handleClick("Clicked fast forward player!")}
-				>
+				<button className={styles.btnFastForward} onClick={handleClick}>
 					<FastForward />
 				</button>
 			</div>
